f67c21b3e3ae7a58a22cf5cbff3968ec6d11860e
Magno <jmagno.dutra@hotmail.com>
2025-01-24 21:06:23 -0300

    Enemy's Movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,17 +3,31 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private Transform waypoint;
+    [SerializeField] private Transform[] waypoint;
 
     private NavMeshAgent agent;
+    private int waypointIndex = 0;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
     }
 
-    private void Start()
+    private void Update()
     {
-        agent.SetDestination(waypoint.position);
+        if (agent.remainingDistance < 0.5f)
+        {
+            agent.SetDestination(GetNextWaypoint());
+        }
+    }
+
+    private Vector3 GetNextWaypoint()
+    {
+        if (waypointIndex >= waypoint.Length) return transform.position;
+
+        Vector3 targetPoint = waypoint[waypointIndex].position;
+        waypointIndex++;
+
+        return targetPoint;
     }
 }
