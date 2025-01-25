b5e882ad27165d6e4a27810f13d7503db4eabc7d
Magno <jmagno.dutra@hotmail.com>
2025-01-24 21:35:41 -0300

    Enemy Rotation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,9 +1,11 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private Transform[] waypoint;
+    [SerializeField] private float turnSpeed = 10;
 
     private NavMeshAgent agent;
     private int waypointIndex = 0;
@@ -11,14 +13,26 @@ public class Enemy : MonoBehaviour
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
     }
 
     private void Update()
     {
+        FaceTarget(agent.steeringTarget);
         if (agent.remainingDistance < 0.5f)
         {
             agent.SetDestination(GetNextWaypoint());
         }
+    }
+
+    private void FaceTarget(Vector3 newTarget)
+    {
+        Vector3 directionToTarget = newTarget - transform.position;
+        directionToTarget.y = 0; // Ignorar rotacao no y
+
+        Quaternion newRotation = Quaternion.LookRotation(directionToTarget);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, turnSpeed * Time.deltaTime);
     }
 
     private Vector3 GetNextWaypoint()
