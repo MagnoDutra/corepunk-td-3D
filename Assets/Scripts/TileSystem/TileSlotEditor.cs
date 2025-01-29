8fde2e8de2bd85971e5fac3768339bc872a94bcf
Magno <jmagno.dutra@hotmail.com>
2025-01-29 00:54:42 -0300

    Extra Tiles, labels, position and rotation in Custom Editor

--- Assets/Scripts/TileSystem/TileSlotEditor.cs
+++ Assets/Scripts/TileSystem/TileSlotEditor.cs
@@ -1,22 +1,82 @@
+using System.Runtime.CompilerServices;
 using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(TileSlot)), CanEditMultipleObjects]
 public class TileSlotEditor : Editor
 {
+    private GUIStyle centeredStyle;
+
     public override void OnInspectorGUI()
     {
         // Serve para atualizar o estado do objeto para vc trabalhar com valores atualizados
         serializedObject.Update();
         base.OnInspectorGUI();
 
-        float buttonWidth = (EditorGUIUtility.currentViewWidth - 25) / 2;
+        centeredStyle = new GUIStyle(GUI.skin.label)
+        {
+            alignment = TextAnchor.MiddleCenter,
+            fontStyle = FontStyle.Bold,
+            fontSize = 14
+        };
+
+        float oneButtonWidth = (EditorGUIUtility.currentViewWidth - 25);
+        float twoButtonWidth = (EditorGUIUtility.currentViewWidth - 25) / 2;
+        float threeButtonWidth = (EditorGUIUtility.currentViewWidth - 25) / 3;
+
+        GUILayout.Label("Position and Rotation", centeredStyle);
 
         // Row 1
         // --------------------------------
         GUILayout.BeginHorizontal();
 
-        if (GUILayout.Button("Field", GUILayout.Width(buttonWidth)))
+        if (GUILayout.Button("Rotate Left", GUILayout.Width(twoButtonWidth)))
+        {
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).RotateTile(-1);
+            }
+        }
+
+        if (GUILayout.Button("Rotate Right", GUILayout.Width(twoButtonWidth)))
+        {
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).RotateTile(1);
+            }
+        }
+
+        GUILayout.EndHorizontal();
+
+        // Row 2
+        // --------------------------------
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("-0.1f on the Y", GUILayout.Width(twoButtonWidth)))
+        {
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).AdjustY(-1);
+            }
+        }
+
+        if (GUILayout.Button("+0.1f on the Y", GUILayout.Width(twoButtonWidth)))
+        {
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).AdjustY(1);
+            }
+        }
+
+        GUILayout.EndHorizontal();
+
+        GUILayout.Label("Tile Options", centeredStyle);
+
+        // Row 3
+        // --------------------------------
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Field", GUILayout.Width(twoButtonWidth)))
         {
             GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileField;
 
@@ -26,7 +86,7 @@ public class TileSlotEditor : Editor
             }
         }
 
-        if (GUILayout.Button("Road", GUILayout.Width(buttonWidth)))
+        if (GUILayout.Button("Road", GUILayout.Width(twoButtonWidth)))
         {
             GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileRoad;
 
@@ -38,10 +98,10 @@ public class TileSlotEditor : Editor
 
         GUILayout.EndHorizontal();
 
-        // Row 2
+        // Row 4
         // --------------------------------
         GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Sideway", GUILayout.Width(buttonWidth * 2)))
+        if (GUILayout.Button("Sideway", GUILayout.Width(oneButtonWidth)))
         {
             GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileSidway;
 
@@ -52,11 +112,13 @@ public class TileSlotEditor : Editor
         }
         GUILayout.EndHorizontal();
 
-        // Row 3
+        GUILayout.Label("Corner Options", centeredStyle);
+
+        // Row 5
         // --------------------------------
         GUILayout.BeginHorizontal();
 
-        if (GUILayout.Button("Inner Corner", GUILayout.Width(buttonWidth)))
+        if (GUILayout.Button("Inner Corner", GUILayout.Width(twoButtonWidth)))
         {
             GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileInnerCorner;
 
@@ -66,9 +128,84 @@ public class TileSlotEditor : Editor
             }
         }
 
-        if (GUILayout.Button("Outer Corner", GUILayout.Width(buttonWidth)))
+        if (GUILayout.Button("Outer Corner", GUILayout.Width(twoButtonWidth)))
         {
             GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileOuterCorner;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+
+        GUILayout.EndHorizontal();
+
+        GUILayout.Label("Bridges and Hills", centeredStyle);
+
+        // Row 6
+        // --------------------------------
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Hill 1", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileHill_1;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+
+        if (GUILayout.Button("Hill 2", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileHill_2;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+
+        if (GUILayout.Button("Hill 3", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileHill_3;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+
+        GUILayout.EndHorizontal();
+
+
+        // Row 7
+        // --------------------------------
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Bridge with field", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileBridgeField;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+
+        if (GUILayout.Button("Bridge with road", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileBridgeRoad;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+
+        if (GUILayout.Button("Bridge with sideway", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().TileBridgeSideway;
 
             foreach (var targetTile in targets)
             {
